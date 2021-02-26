974fb353f3aec9a2c83dfc730ec98e8f733ee742
Rafał Karczmarzyk <liptunred@gmail.com>
2021-02-26 21:29:10 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import './icons.font'
+
+import './styles/reset.scss'
+import './styles/App.scss'
+
+import Router from './Router'
+import configureStore from './store'
+const store = configureStore()
+
+const appRoot = (
+    <Provider store={store}>
+        <Router />
+    </Provider>
+)
+
+ReactDOM.render(appRoot, document.getElementById('root'))
